17a37dc823188fc5c25c7015c8c70c9fc62ce6af
NatashaKapeyi <natashazikhona7@gmail.com>
2023-11-24 15:07:30 +0200

    push

--- script/code.js
+++ script/code.js
@@ -1,3 +1,15 @@
 // convert minutes into seconds 
+const minutes = document.querySelector('#minutes');
+    const result= document.querySelector('#result');
+let button = document.querySelector('.button');
+button.addEventListener('click', convert)
 
-convertMtoS = (minutes) => 60*minutes
+function convert(){
+    
+    const convert = minutes.value * 60
+    result.textContent= (minutes.value * 60 );
+
+
+}
+
+
